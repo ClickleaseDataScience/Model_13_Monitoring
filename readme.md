d3201d94763b1b7da304e7254ac1ce4b31510696
EC2 Default User <ec2-user@ip-172-31-16-47.ec2.internal>
2024-12-09 23:04:37 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Readme 
+
+The goal of this repo is to train a model to detect differences between Model 13 training data and produciton data. 
+
---
+++ tmp.py
@@ -0,0 +1,24 @@
+print("hello world")
+
+import pandas as pd
+
+['experian_experianProfileSummary_installmentBalance',
+       'experian_experianProfileSummary_totalRevolvingBalance',
+       'experian_experianProfileSummary_totalRevolvingAvailablePercent',
+       'experian_experianProfileSummary_pastDueAmount',
+       'experian_experianProfileSummary_monthlyPayment',
+       'experian_experianProfileSummary_inquiriesDuringLastSixMonthsCounter',
+       'experian_experianProfileSummary_totalInquiriesCounter',
+       'experian_experianProfileSummary_totalTradeItemsCounter',
+       'experian_experianProfileSummary_paidAccountsCounter',
+       'experian_experianProfileSummary_wasDelinquentDerogCounter',
+       'experian_experianProfileSummary_thirtyDayCounter',
+       'experian_experianProfileSummary_ninetyPlusDayCounter',
+       'experian_experianProfileSummary_derogCounter',
+       'experian_experianRiskModel_V3', 
+       'experian_experianRiskModel_RC',
+       'datax_dataxReport_idaScore',
+       'coreLogic_coreLogicInquiryReport_corelIdScore',
+       'coreLogic_coreLogicApplicantVariable_TTInqall00',
+       'coreLogic_coreLogicApplicantVariable_TTRecinqm', 
+       'vendor_equipment_type_all']
